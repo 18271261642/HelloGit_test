33dac4187ffea3af3b684d9b6e499a0e27fa389a
sjh18271261642 <18271261642@163.com>
2017-08-28 15:43:56 +0800

    增加点击按钮的功能

--- app/src/main/java/com/android/hellogittest/MainActivity.java
+++ app/src/main/java/com/android/hellogittest/MainActivity.java
@@ -2,6 +2,9 @@ package com.android.hellogittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.testBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"the button clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
